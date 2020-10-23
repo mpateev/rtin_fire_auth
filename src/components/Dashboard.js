b91cae7a13b69fbe29ee5833b7da3ef22517e536
Pateev <murad.pateev@dornier-consulting.com>
2020-10-23 10:32:36 +0200

    Completed Auth

---
+++ src/components/Dashboard.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Alert, Button, Card } from "react-bootstrap";
+import { useAuth } from "../contexts/AuthContext";
+
+export default function Dashboard() {
+  const [error, setError] = useState("");
+  const { currentUser, logout } = useAuth();
+  const history = useHistory();
+
+  async function handleLogOut() {
+    setError("");
+    try {
+      await logout();
+      history.push("/login");
+    } catch {
+      setError("Failed to log out!");
+    }
+  }
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Profile</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email:</strong>&nbsp;
+          {currentUser.email}
+          <Link to="/update-profile" className="btn btn-primary w-100 mt-3">
+            Update Profile
+          </Link>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogOut}>
+          Log Out
+        </Button>
+      </div>
+    </>
+  );
+}
